a27f4d6ea1c61a6916aa8b03c015b9029cb6a2ca
ryym <ryym.64@gmail.com>
2017-06-03 10:36:44 +0900

    Remove auto-generated code from KV module

--- lib/kv.ex
+++ lib/kv.ex
@@ -1,18 +1,2 @@
 defmodule KV do
-  @moduledoc """
-  Documentation for KV.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KV.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
